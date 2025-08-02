9f3393473313741f9cd310edeed1f8425fcd46ec
Lucky Raj <kumari.lucky.raj.cse.2023@tint.edu.in>
2025-08-02 20:14:30 +0530

    tut1 to tut4

---
+++ tut1.py
@@ -0,0 +1,10 @@
+# hello
+my_str ='Lucky' 
+print(my_str.isalnum())
+lst = [1 ,'hey','okay']
+lst.append(['lucky','raj'])#nested list
+print(lst.count(['lucky','raj']))
+set_var={1,2,3,4,3} # no indexing is allowed in sets
+print(set_var)
+#dict() (empty dictionary) key= index
+my_tuple =(1,4,6,8)# immutable, used only once
